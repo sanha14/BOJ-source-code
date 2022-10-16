933038b06ffcc91563aabd3e0f52388f215097c7
Yoon Johyun <porory415@gmail.com>
2022-10-16 12:08:14 +0900

    22/10/16 3 problems are solved

---
+++ BOJ_silver/11047.py
@@ -0,0 +1,18 @@
+# 동전0
+
+value = list(map(int, input().split()))
+coin = []
+
+for i in range(value[0]):
+	coin.append(int(input()))
+
+coin.sort(reverse=True)
+result = 0
+for i in coin:
+	if value[1] == 0:
+		break
+	else:
+		result += value[1] // i
+		value[1] = value[1] % i
+
+print(result)
